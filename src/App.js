8f5dddc9d798c4307b42bb33918f411d61fca745
Ajit Kumar Gupta <ajit45288@gmail.com>
2021-10-11 20:47:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+import logo from "./logo.svg";
+import "./styles.css";
+import Weather from "./components/Weather";
+
+function App() {
+  return <Weather />;
+}
+export default App;
